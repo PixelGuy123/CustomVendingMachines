1046d6a7203362cd0f0fb6321fe83d6451cec0a7
PixelGuy123 <89644121+PixelGuy123@users.noreply.github.com>
2025-01-14 14:37:59 -0300

    Refactored the whole code

--- Plugin.cs
+++ Plugin.cs
@@ -1,23 +1,25 @@
 ﻿using BepInEx;
-using BepInEx.Bootstrap;
 using HarmonyLib;
-using MTM101BaldAPI;
 using MTM101BaldAPI.AssetTools;
 using MTM101BaldAPI.Registers;
 using PixelInternalAPI;
-using PixelInternalAPI.Extensions;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
 using UnityEngine;
+using Newtonsoft.Json;
+using BepInEx.Bootstrap;
+using MTM101BaldAPI;
+using PixelInternalAPI.Extensions;
+using System.Threading;
 
 namespace CustomVendingMachines
 {
 	[BepInDependency("mtm101.rulerp.bbplus.baldidevapi", BepInDependency.DependencyFlags.HardDependency)]
 	[BepInDependency("pixelguy.pixelmodding.baldiplus.pixelinternalapi", BepInDependency.DependencyFlags.HardDependency)]
-	[BepInPlugin("pixelguy.pixelmodding.baldiplus.customvendingmachines", PluginInfo.PLUGIN_NAME, "1.0.4.2")]
+	[BepInPlugin("pixelguy.pixelmodding.baldiplus.customvendingmachines", PluginInfo.PLUGIN_NAME, "1.0.5")]
 	public class CustomVendingMachinesPlugin : BaseUnityPlugin
 	{
 		// *** Use this method for your mod to add custom vending machines ***
@@ -35,13 +37,13 @@ namespace CustomVendingMachines
 
 				try
 				{
-					datas.Add(new(path, JsonUtility.FromJson<VendingMachineData>(File.ReadAllText(file))));
+					datas.Add(new(path, JsonConvert.DeserializeObject<VendingMachineData>(File.ReadAllText(file))));
 				}
 				catch (Exception e)
 				{
-					Debug.LogWarning("BBCustomVendingMachines: Failed to load json!: " + Path.GetFileName(file));
+					Debug.LogWarning("BBCustomVendingMachines: Failed to load json from path: " + Path.GetFileName(file));
 					Debug.LogException(e);
-					errors.Add("Failed to load json!: " + Path.GetFileName(file));
+					errors.Add("Failed to load json: " + Path.GetFileName(file));
 				}
 			}
 		}
@@ -50,50 +52,136 @@ namespace CustomVendingMachines
 			Harmony h = new("pixelguy.pixelmodding.baldiplus.customvendingmachines");
 			h.PatchAll();
 
+			//File.WriteAllText(Path.Combine(AssetLoader.GetModPath(this), "test.json"), JsonConvert.SerializeObject(new VendingMachineData()
+			//{
+			//	acceptableItems = ["someItem", "someitem2"],
+			//	allowedLevels = ["F1", "F2", "F3", "END", "INF5"],
+			//	items = [
+			//		new() { item = "Quarter"},
+			//		new() { item = "Present", weight = 50},
+			//		],
+			//}, Formatting.Indented));
+
 			AddDataFromDirectory(AssetLoader.GetModPath(this)); // Read from the directory already
+
+			Structure_EnvironmentObjectPlacer placer = null;
 
 			GeneratorManagement.Register(this, GenerationModType.Addend, (name, num, sco) =>
 			{
-				var ld = sco.levelObject;
-				if (ld == null)
-					return;
-
 				LoadVendingMachines();
 
-				bool endless = name == "INF";
-				for (int i = num; !endless || i >= lastlevelnum; i--)
+				if (!placer)
+					 placer = GenericExtensions.FindResourceObjectByName<Structure_EnvironmentObjectPlacer>("Structure_EnvironmentObjectBuilder_Weighted");
+
+				if (!sco.levelObject)
+					return;
+
+				List<WeightedGameObject> machines = [];
+				bool infiniteFloors = name.StartsWith("INF");
+
+				foreach (var machine in sodaMachines)
 				{
-					foreach (var machine in sodaMachines)
+					bool flag = false;
+					if (infiniteFloors)
 					{
-						if (!machine.IncludeInLevel(endless ? name + i.ToString() : name)) continue;
-						ItemObject item = !Chainloader.PluginInfos.TryGetValue(machine.modId, out var inf) ? // If no Info found, findByNormalEnum
-							ItemMetaStorage.Instance.FindByEnum(machine.en).value :
-							ItemMetaStorage.Instance.FindByEnumFromMod(machine.en, inf).value;
-
-						machine.machine.SetPotentialItems(new WeightedItemObject() { selection = item, weight = 1 })
-						.SetUses(machine.usesLeft);
-
-
-						StructureWithParameters param = ld.forcedStructures.FirstOrDefault(s => s.prefab is Structure_EnvironmentObjectPlacer);
-						if (param == null)
-							ld.potentialStructures.FirstOrDefault(s => s.selection.prefab is Structure_EnvironmentObjectPlacer);
-
-						if (param == null) // If it's still null... then skip
-							continue;
-
-
-						param.parameters.prefab = param.parameters.prefab.AddToArray(new() { selection = machine.machine.gameObject, weight = machine.sodaMachineWeight });
-						param.parameters.minMax[0].x += machine.minAmount;
-						param.parameters.minMax[0].z += machine.maxAmount;
+						for (int i = 0; i < machine.allowedLevels.Length; i++)
+						{
+							int res = -1;
+							if (machine.allowedLevels[i].StartsWith("INF") && 
+							int.TryParse(machine.allowedLevels[i].Substring(3, machine.allowedLevels[i].Length - 3), out res) && // Gets the raw number after the "INF" prefix
+							num >= res // Inf floor support
+							)
+							{
+								flag = true;
+								break;
+							}
+						}
 					}
-					if (!endless)
+					else if (machine.allowedLevels.Contains(name))
+						flag = true;
+					
+					if (flag)
+						machines.Add(new() { selection = machine.machine.gameObject, weight = machine.sodaMachineWeight });
+					
+
+				}
+
+				if (machines.Count == 0) 
+					return;
+
+				WeightedGameObject[] machinesArray = [.. machines];
+
+				if (infiniteFloors) 
+				{
+					sco.levelObject.forcedStructures = sco.levelObject.forcedStructures.AddToArray(new() 
+					{ prefab = placer, 
+					parameters = new() 
+						{ 
+							minMax = [new(Mathf.FloorToInt(2 * num * 0.35f), Mathf.FloorToInt(3.5f * num * 0.45f))],
+							chance = [0.25f * num * 0.35f % 1f],
+							prefab = machinesArray
+					} 
+					});
+					return;
+				}
+
+				switch (name)
+				{
+					default: break;
+
+					case "F1":
+						sco.levelObject.forcedStructures = sco.levelObject.forcedStructures = sco.levelObject.forcedStructures.AddToArray(new()
+						{
+							prefab = placer,
+							parameters = new()
+							{
+								minMax = [new(2, 5)],
+								chance = [0.5f],
+								prefab = machinesArray
+							}
+						});
+						break;
+
+					case "F2":
+						sco.levelObject.forcedStructures = sco.levelObject.forcedStructures = sco.levelObject.forcedStructures.AddToArray(new()
+						{
+							prefab = placer,
+							parameters = new()
+							{
+								minMax = [new(4, 6)],
+								chance = [0.35f],
+								prefab = machinesArray
+							}
+						});
+						break;
+
+					case "F3":
+						sco.levelObject.forcedStructures = sco.levelObject.forcedStructures = sco.levelObject.forcedStructures.AddToArray(new()
+						{
+							prefab = placer,
+							parameters = new()
+							{
+								minMax = [new(6, 9)],
+								chance = [0.65f],
+								prefab = machinesArray
+							}
+						});
+						break;
+
+					case "END":
+						sco.levelObject.forcedStructures = sco.levelObject.forcedStructures = sco.levelObject.forcedStructures.AddToArray(new()
+						{
+							prefab = placer,
+							parameters = new()
+							{
+								minMax = [new(4, 7)],
+								chance = [0.5f],
+								prefab = machinesArray
+							}
+						});
 						break;
 				}
-				
-				ld.MarkAsNeverUnload();
-				lastlevelnum = num;
 			});
-
 		}
 
 		void LoadVendingMachines()
@@ -101,80 +189,94 @@ namespace CustomVendingMachines
 			if (initializedMachines) return;
 			initializedMachines = true;
 
-			int sodaCount = GenericExtensions.FindResourceObjects<SodaMachine>().Length; // amount of soda machine prefabs
-
-			foreach (var data in datas)
+			static void ThrowIfInvalidTexture(Texture2D tex)
 			{
-				Texture2D normaltex;
-				Texture2D outTex = null;
+				if (tex.width != 128 || tex.height != 224)
+					throw new ArgumentOutOfRangeException($"Texture ({tex.name}) has an invalid height or width ({tex.width}x{tex.height})");
+			}
+
+			foreach (var dataPair in datas)
+			{				
+				var data = dataPair.Value;
+				
 				try
 				{
-					Items en = EnumExtensions.GetFromExtendedName<Items>(data.Value.itemName);
-					Chainloader.PluginInfos.TryGetValue(data.Value.modId, out BepInEx.PluginInfo inf);
+					// Texture Load
+					string texPath = Path.Combine(dataPair.Key, data.normalTextureFileName);
+					if (!File.Exists(texPath))
+						throw new FileNotFoundException($"Failed to find the main texture for the vending machine ({Path.GetFileName(texPath)})");
 
-					if (!ItemMetaStorage.Instance.All().Any(x => x.id == en && (inf == null || x.info == inf)))
-						throw new InvalidItemsEnumException(data.Value.itemName);
+					Texture2D normalTexture = AssetLoader.TextureFromFile(texPath);
+					ThrowIfInvalidTexture(normalTexture);
 
-					if (data.Value.usesLeft == 0) // Lotta of checks lol
-						throw new ArgumentException("the usesLeft being set to 0");
-					if (data.Value.minAmount < 0)
-						throw new ArgumentException("the minAmount being below 0");
-					if (data.Value.minAmount > data.Value.maxAmount)
-						throw new ArgumentException("the minAmount being higher than maxAmount");
-					if (data.Value.sodaMachineWeight < 0)
-						throw new ArgumentException("the sodaMachineWeight being below 0");
+					Texture2D outOfStockTex = null;
 
-					if (!File.Exists(Path.Combine(data.Key, data.Value.normalTextureFileName)))
-						throw new ArgumentException("a missing normal texture. Texture name: " + data.Value.normalTextureFileName);
-
-					normaltex = AssetLoader.TextureFromFile(Path.Combine(data.Key, data.Value.normalTextureFileName));
-					if (normaltex.width != 128 || normaltex.height != 224)
-						throw new ArgumentException($"an invalid texture: {normaltex.width}/{normaltex.height} | Expected size: 128/224 >> Texture Name: {data.Value.normalTextureFileName}");
-
-					if (data.Value.usesLeft > 0) // The out texture will be ignored if the vending machine has infinite uses (uses < 0 are considered infinite)
+					if (!string.IsNullOrEmpty(data.outOfStockTextureFileName)) 
 					{
-						if (!File.Exists(Path.Combine(data.Key, data.Value.outOfStockFileName)))
-							throw new ArgumentException("a missing outOfStock texture. Texture name: " + data.Value.outOfStockFileName);
+						texPath = Path.Combine(dataPair.Key, data.outOfStockTextureFileName);
+						if (!File.Exists(texPath))
+							throw new FileNotFoundException($"Failed to find the out of stock texture for the vending machine ({Path.GetFileName(texPath)})");
 
-						outTex = AssetLoader.TextureFromFile(Path.Combine(data.Key, data.Value.outOfStockFileName));
-						if (outTex.width != 128 || outTex.height != 224)
-							throw new ArgumentException($"an invalid texture: {outTex.width}/{outTex.height} | Expected size: 128/224 >> Texture Name: {data.Value.outOfStockFileName}");
+						outOfStockTex = AssetLoader.TextureFromFile(texPath);
+						ThrowIfInvalidTexture(outOfStockTex);
 					}
 
-					data.Value.en = en;
-				}
-				catch(ArgumentException e)
-				{
-					errors.Add($"Failed to load vending machine (\"{data.Value.itemName}\") due to {e.Message}");
-					Debug.LogWarning($"BBCustomVendingMachines: Failed to load vending machine ({data.Value.itemName}) due to {e.Message}");
-					continue;
-				}
-				catch (InvalidItemsEnumException e)
-				{
-					errors.Add($"Failed to load vending machine due to {e.Message}");
-					Debug.LogWarning($"BBCustomVendingMachines: Failed to load vending machine due to {e.Message}");
-					continue;
+					// Item search
+					WeightedItemObject[] weightedItems = new WeightedItemObject[data.items.Length];
+					for (int i = 0; i < data.items.Length; i++)
+					{
+						if (data.items[i].item == "None" || string.IsNullOrEmpty(data.items[i].item))
+							throw new InvalidItemsEnumException(data.items[i].item);
+
+						var itmEnum = EnumExtensions.GetFromExtendedName<Items>(data.items[i].item);
+						var itemMeta = (string.IsNullOrEmpty(data.items[i].mod_guid) ?
+								ItemMetaStorage.Instance.FindByEnum(itmEnum) : ItemMetaStorage.Instance.FindByEnumFromMod(itmEnum, Chainloader.PluginInfos[data.items[i].mod_guid]))
+
+								?? throw new InvalidItemsEnumException(data.items[i].item); // if itemMeta is null, throw this error
+
+						weightedItems[i] = new WeightedItemObject()
+						{
+							weight = data.items[i].weight,
+							selection = itemMeta.value
+						};
+					}
+
+					data.machine = ObjectCreationExtensions.CreateSodaMachineInstance(normalTexture, outOfStockTex ?? normalTexture);
+					data.machine.AddNewPotentialItems(weightedItems);
+
+					// Acceptable items search
+					if (data.acceptableItems.Length != 0)
+					{
+						List<Items> acceptableItms = [];
+						for (int i = 0; i < data.acceptableItems.Length; i++)
+						{
+							if (string.IsNullOrEmpty(data.acceptableItems[i]))
+								throw new InvalidItemsEnumException(data.acceptableItems[i]);
+
+							try
+							{
+								acceptableItms.Add(EnumExtensions.GetFromExtendedName<Items>(data.acceptableItems[i]));
+							}
+							catch
+							{
+								Debug.LogWarning($"Failed to find enum for acceptable item ({data.acceptableItems[i]}).");
+							}
+						}
+						if (acceptableItms.Count != 0)
+							data.machine.AddNewRequiredItems([.. acceptableItms]);
+					}
+
+					sodaMachines.Add(data);
 				}
 				catch(Exception e)
 				{
-					errors.Add($"Failed to load vending machine due to a specific bug or inexistent Items enum: \"{data.Value.itemName}\"");
-					Debug.LogWarning($"BBCustomVendingMachines: Failed to load vending machine due to a specific bug or inexistent Items enum: {data.Value.itemName}");
 					Debug.LogException(e);
-					continue;
+					errors.Add(e.Message);
 				}
-
-				var sodaMachine = ObjectCreationExtensions.CreateSodaMachineInstance(normaltex
-					, outTex);
-				// Include soda machine as a prefab of course, so it appears
-				sodaMachine.name = $"{data.Value.itemName}SodaMachine";
-				data.Value.machine = sodaMachine;
-
-				//.Instance.Add(new(Info, vendingMachineBuilder)); Meta data for structure builders... if there was one lol
-
-				sodaMachines.Add(data.Value);
-				prefabs.Add(sodaMachine.gameObject);
-				sodaCount++;
 			}
+
+			
+
 
 			StringBuilder accerrors = new();
 			for (int i = 0; i < errors.Count;)
@@ -186,12 +288,9 @@ namespace CustomVendingMachines
 					accerrors.Clear();
 				}
 			}
+
 			if (accerrors.Length > 0)
 				ResourceManager.RaisePopup(Info, accerrors.ToString());
-
-			// Balance out weights
-			if (sodaCount != 0)
-				sodaMachines.ForEach(mac => mac.sodaMachineWeight /= sodaCount);
 		}
 
 		static bool initializedMachines = false;
@@ -203,55 +302,45 @@ namespace CustomVendingMachines
 		readonly internal List<VendingMachineData> sodaMachines = [];
 
 		readonly static List<KeyValuePair<string, VendingMachineData>> datas = [];
-
-		readonly static internal List<GameObject> prefabs = [];
 	}
 
-	class InvalidItemsEnumException(string invalidEnumName) : Exception($"an Item that doesn\'t exist in the meta storage ({invalidEnumName})");
+	class InvalidItemsEnumException(string invalidEnumName) : Exception($"An Item that doesn\'t exist in the meta storage ({invalidEnumName})");
 
-	[Serializable]
+	[JsonObject]
 	class VendingMachineData
 	{
-		public string modId = string.Empty;
-		public int minAmount = 1;
-		public int maxAmount = 3;
+		public VendingMachineData() =>
+			usesLeft = Mathf.Max(usesLeft, 1);
+
+		[JsonRequired]
 		public int sodaMachineWeight = 100;
+
 		public int usesLeft = 1;
+		[JsonRequired]
 		public string normalTextureFileName = string.Empty;
-		public string outOfStockFileName = string.Empty;
-		public string itemName = string.Empty;
+
+		public string outOfStockTextureFileName = string.Empty;
+
+		[JsonRequired]
+		public VendingMachineItem[] items = [];
+
+		public string[] acceptableItems = [];
+
+		[JsonRequired]
 		public string[] allowedLevels = [];
-		public bool IncludeInLevel(string floor) => // A BUNCH OF CHECKS
-			allowedLevels.Contains(floor);
 
-		[NonSerialized]
+		[JsonIgnore]
 		public SodaMachine machine;
-
-		[NonSerialized]
-		public Items en;
 	}
 
-	/*
-	class ModdedSaveThingy : ModdedSaveGameIOText
+	[JsonObject]
+	class VendingMachineItem
 	{
-		public override BepInEx.PluginInfo pluginInfo => Plugin.i.Info;
-		public override void LoadText(string toLoad) =>
-			Plugin.queuedBuilders = toLoad;
-		
-
-		public override string SaveText()
-		{
-			StringBuilder bld = new();
-			foreach (var b in Plugin.alreadyIncludedBuilders)
-				bld.Append(b.selection.name + "/");
-			return bld.ToString();
-		}
-
-		public override void Reset()
-		{
-		}
+		[JsonRequired]
+		public string item = "None";
+		public int weight = 100;
+		public string mod_guid = string.Empty;
 	}
-	*/
 
 	// When you quit the game, it should always lead to this screen anyways
 	[HarmonyPatch(typeof(MainMenu), "Start")]
